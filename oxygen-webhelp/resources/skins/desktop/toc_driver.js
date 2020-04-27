03d8835ccdd5a57af9e2f0e622650475da309abe
Farshad Ghassemi Toosi <farshad.toosi@COM-B180-M32373.home>
2020-04-27 18:59:56 +0100

    first

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20200427065242", "oxygen-webhelp/search/index-1.js?uniqueId=20200427065242", "oxygen-webhelp/search/index-2.js?uniqueId=20200427065242", "oxygen-webhelp/search/index-3.js?uniqueId=20200427065242"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20200427065824", "oxygen-webhelp/search/index-1.js?uniqueId=20200427065824", "oxygen-webhelp/search/index-2.js?uniqueId=20200427065824", "oxygen-webhelp/search/index-3.js?uniqueId=20200427065824"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
