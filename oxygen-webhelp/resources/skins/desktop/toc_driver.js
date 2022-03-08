bc849c8136be4b5b1c47cf857d6a33473bf4ce32
farshad1982 <farshad.toosi@cit.ie>
2022-03-08 09:49:56 +0000

    editing diet

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20220214101047", "oxygen-webhelp/search/index-1.js?uniqueId=20220214101047", "oxygen-webhelp/search/index-2.js?uniqueId=20220214101047", "oxygen-webhelp/search/index-3.js?uniqueId=20220214101047"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20220308094755", "oxygen-webhelp/search/index-1.js?uniqueId=20220308094755", "oxygen-webhelp/search/index-2.js?uniqueId=20220308094755", "oxygen-webhelp/search/index-3.js?uniqueId=20220308094755"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
