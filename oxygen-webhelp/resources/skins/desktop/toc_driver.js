3e4382f403560df221677b991f90b9993a1dc4e8
farshad1982 <farshad.toosi@cit.ie>
2022-03-11 00:58:28 +0000

    update on diet

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20220308094755", "oxygen-webhelp/search/index-1.js?uniqueId=20220308094755", "oxygen-webhelp/search/index-2.js?uniqueId=20220308094755", "oxygen-webhelp/search/index-3.js?uniqueId=20220308094755"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20220311125644", "oxygen-webhelp/search/index-1.js?uniqueId=20220311125644", "oxygen-webhelp/search/index-2.js?uniqueId=20220311125644", "oxygen-webhelp/search/index-3.js?uniqueId=20220311125644"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
